2bd9683f64fe957de6a3573466769543665cb50d
aaronbush100 <aaronbush100@gmail.com>
2017-05-07 14:42:51 -0400

    work

---
+++ data/friends.js
@@ -0,0 +1,15 @@
+//will hold all of the data for new members
+
+var friends = [
+
+	{
+		name: "Aaron",
+		location:"DC",
+		age: "22",
+		photoLink: "",
+		score: "",		
+	},
+
+]
+
+module.exports = friends; //makes table data available to other pages
